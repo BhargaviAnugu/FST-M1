8262d108011d33847be4dfe014211e2695cd28fa
BhargaviAnugu <37537265+BhargaviAnugu@users.noreply.github.com>
2026-02-02 10:57:33 +0530

    Add files via upload

--- Cucumber/Activities/BaseClass.java
+++ Cucumber/Activities/BaseClass.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class BaseClass {
 
-	    // Create a new instance of the Firefox driver
+	    // Create a new instance of the Fire fox driver
 	    static WebDriver driver;
 	    static WebDriverWait wait;
 }
